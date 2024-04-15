b323dc870b80a0d8ca83843f4616cc43af214dc8
Bayarmagnai <bayarlvl@gmail.com>
2024-04-16 00:47:49 +0800

    Asuudluud garch irev gehdee zasah heregtei

--- assets/js/Controllers/elementSizeChangeController.js
+++ assets/js/Controllers/elementSizeChangeController.js
@@ -23,56 +23,58 @@
 const whitePageWay = window.parent.document.getElementById('render');
 
 
-whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
+whitePageWay.contentWindow.addEventListener('click', (event) => {
     var selectElement = event.target;
-    if (selectElement.className.includes('selectable')) {
+    
+    if (selectElement.className.includes('selectable')) {   
         const insideWhitePage = whitePageWay.contentWindow.document.getElementById("whitePage");
-        
-        // cursoriin helberjilt bairshilaas hamarch uurchlugduh
-     insideWhitePage.onmousemove = function(event) {
-  
-
-            var coordinate = selectElement.getBoundingClientRect();
-            var x = event.pageX;
-            var y = event.pageY;
-            var selectedElementHeight = coordinate.height;
-            var selectedElementWidth = coordinate.width;
-            var xleftcorner = coordinate.x;
-            var yleftcorner = coordinate.y;
+        function changeSizeAndPosition(){
             
-            if (x > xleftcorner - 10 && x < xleftcorner + 13 && yleftcorner - 13 < y && y < yleftcorner + 13){
+            // cursoriin helberjilt bairshilaas hamarch uurchlugduh
+            insideWhitePage.onmousemove = function(event) {
+                
+                
+                var coordinate = selectElement.getBoundingClientRect();
+                var x = event.pageX;
+                var y = event.pageY;
+                var selectedElementHeight = coordinate.height;
+                var selectedElementWidth = coordinate.width;
+                var xleftcorner = coordinate.x;
+                var yleftcorner = coordinate.y;
+                
+                if (x > xleftcorner - 10 && x < xleftcorner + 13 && yleftcorner - 13 < y && y < yleftcorner + 13){
+                    insideWhitePage.style.cursor = 'nwse-resize';
+                    insideWhitePage.onmousedown = function() {
+                        insideWhitePage.onmousemove = function(e) {
+                            x = e.pageX;
+                            y = e.pageY;
+                            selectElement.style.width = x - xleftcorner + 'px';
+                            selectElement.style.height = y - yleftcorner + 'px';
+                        }
+                    }
+                    
+                    insideWhitePage.onmouseup = function () {
+                        insideWhitePage.onmousemove = function(){
+                        }
+                    }
+                    
+                    
+                }
+                else if (x > xleftcorner + selectedElementWidth - 10 && x < xleftcorner + selectedElementWidth + 10 && yleftcorner + selectedElementHeight - 10 < y && y < yleftcorner + selectedElementHeight + 10) {
                 insideWhitePage.style.cursor = 'nwse-resize';
+                selectElement.style.cursor = 'nwse-resize';
                 insideWhitePage.onmousedown = function() {
                     insideWhitePage.onmousemove = function(e) {
                         x = e.pageX;
                         y = e.pageY;
-                        selectElement.style.width = x - xleftcorner + 'px';
-                        selectElement.style.height = y - yleftcorner + 'px';
-                    }
-                }
-
-                insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
-                }
-                
-                       
-            }
-            else if (x > xleftcorner + selectedElementWidth - 10 && x < xleftcorner + selectedElementWidth + 10 && yleftcorner + selectedElementHeight - 10 < y && y < yleftcorner + selectedElementHeight + 100) {
-                insideWhitePage.style.cursor = 'nwse-resize';
-                
-                insideWhitePage.onmousedown = function() {
-                    insideWhitePage.onmousemove = function(e) {
-                        x = e.pageX;
-                        y = e.pageY;
                         selectElement.style.height = y - yleftcorner + 'px';
                         selectElement.style.width = x - xleftcorner + 'px';
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                    }
                 }
                 
             }
@@ -86,10 +88,11 @@ whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
                         selectElement.style.width = x - xleftcorner + 'px';
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                        
+                    }
                 }
             }
             else if (x > xleftcorner - 13 && x < xleftcorner + 13 && yleftcorner + selectedElementHeight - 13 < y && y < yleftcorner + selectedElementHeight + 13) {
@@ -102,10 +105,11 @@ whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
                         selectElement.style.width = selectedElementWidth + xleftcorner - x + 'px';
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                        
+                    }
                 }
             }
             else if (x > xleftcorner + 8 && x < xleftcorner + selectedElementWidth - 8 && yleftcorner - 8 < y && y < yleftcorner + 8) {
@@ -117,10 +121,12 @@ whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
                         selectElement.style.height = selectedElementHeight + y - yleftcorner + 'px';
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                        
+
+                    }
                 }
             }
             else if (x > xleftcorner + 8 && x < xleftcorner + selectedElementWidth - 8 && yleftcorner + selectedElementHeight - 8 < y && y < yleftcorner + selectedElementHeight + 8) {
@@ -132,10 +138,11 @@ whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
                         selectElement.style.height = y - yleftcorner + 'px';
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                        
+                    }
                 }
                 
             }
@@ -146,16 +153,18 @@ whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
                         x = e.pageX;
                         y = e.pageY;
                         selectElement.style.width = selectedElementWidth - x + xleftcorner + 'px';
-
+                        
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                        
+                        
+                    }
                 }
                 
-               
+                
             }
             else if (x < xleftcorner + selectedElementWidth + 8 && x > xleftcorner + selectedElementWidth - 8 && yleftcorner + 8 < y && y < yleftcorner + selectedElementHeight - 8) {
                 selectElement.style.cursor = 'ew-resize';
@@ -166,51 +175,59 @@ whitePageWay.contentWindow.addEventListener('mousedown', (event) => {
                         selectElement.style.width = x - xleftcorner + 'px';
                     }
                 }
-
+                
                 insideWhitePage.onmouseup = function () {
-                   insideWhitePage.onmousemove = function(){
-                   }
+                    insideWhitePage.onmousemove = function(){
+                        
+                    }
                 }   
-                    
-
-            }else if(x>xleftcorner && x<xleftcorner+selectedElementWidth && y>yleftcorner && y<yleftcorner+selectedElementHeight){
-                    selectElement.style.cursor = 'all-scroll';
-                    selectElement.style.position = 'absolute';
-                    selectElement.onmousedown = function () {
-                        selectElement.onmousemove = function(e) {
-                            x = e.pageX;
-                            y = e.pageY;
-                            selectElement.style.left = x - 100 + "px";
-                            selectElement.style.top = y - 100 + "px";
-                        }
+                
+                
+            }else{
+                selectElement.style.cursor = 'all-scroll';
+                selectElement.style.position = 'absolute';
+                selectElement.onmousedown = function () {
+                    selectElement.onmousemove = function(e) {
+                        x = e.pageX;
+                        y = e.pageY;
+                        selectElement.style.left = x - selectedElementWidth/2 + "px";
+                        selectElement.style.top = y - selectedElementHeight/2 + "px";
                     }
-    
-                    selectElement.onmouseup = function () {
-                       selectElement.onmousemove = function(){
-                       }
-                    }
-                }else{
-                    insideWhitePage.style.cursor = "default"
                 }
-
+                
+                selectElement.onmouseup = function () {
+                    selectElement.onmousedown = function(){
+                        
+                    }
+                    selectElement.onmousemove = function(){
+                        
+                    }
+                    changeSizeAndPosition()
+                }
+                
             }
-        }   
-    });
-// if (selectElement.classList.contains('selectable')) {
-//     selectElement.style.outline = "5px solid #87CEFA"
-// notSelectDiv = selectElement.tagName.toLowerCase().substring(0, 3);
-// if (notSelectDiv.includes('div')) {
-//     selectElement.style.width="100%"
-// } else {
-//     //Zuuh uyed duudah function
-//     let chooseElement = null;
-//     console.log(chooseElement)
-//         selectElement.style.position = "absolute";
-//         chooseElement = selectElement;
-//         document.onmousemove = (e) => {
-//              x = e.pageX;
-//              y = e.pageY;
             
+        }
+        }
+        changeSizeAndPosition()
+    }  
+});
+selectElement=null;
+// if (selectElement.classList.contains('selectable')) {
+    //     selectElement.style.outline = "5px solid #87CEFA"
+    // notSelectDiv = selectElement.tagName.toLowerCase().substring(0, 3);
+    // if (notSelectDiv.includes('div')) {
+        //     selectElement.style.width="100%"
+        // } else {
+            //     //Zuuh uyed duudah function
+            //     let chooseElement = null;
+            //     console.log(chooseElement)
+            //         selectElement.style.position = "absolute";
+            //         chooseElement = selectElement;
+            //         document.onmousemove = (e) => {
+                //              x = e.pageX;
+                //              y = e.pageY;
+                    
 //             chooseElement.style.left = x - 50 + "px";
 //             chooseElement.style.top = y - 10 + "px";
 //         }
