575437d28c550164dfadae180b39d5a874cee3d8
Bayaraa <bayarlvl@gmail.com>
2024-04-23 18:12:06 +0800

    code hesgiin beldets duussan

--- assets/js/Controllers/codePartController.js
+++ assets/js/Controllers/codePartController.js
@@ -1,12 +1,20 @@
-const codeContext = document.querySelector(".codePage");
-const renderContext = document.querySelector(".whitePage");
-const shifterLever = document.querySelector(".shifterPart");
-codeContext.style.display =  "flex";
-renderContext.style.height = "95vh";   
-console.log(shifterLever)
-    shifterLever.onclick = function(){
-       
-       renderContext.style.height = renderContext.style.height === "95vh" ? "45vh" : "95vh";
-       codeContext.style.display = codeContext.style.display === "none" ? "flex" : "none";
 
-    }
+const codingSection =  window.parent.document.querySelector(".codeSection");
+const shifterLever = document.querySelector(".shifterPart");
+const renderContext = window.parent.document.querySelector(".whitePage");
+const codeContext = document.querySelector(".codeWindows");
+
+console.log(codeContext)
+
+shifterLever.onclick = function () {
+   const codeContext = codingSection.contentWindow.document.querySelector(".codeWindows");
+
+   console.log(codeContext)
+   renderContext.style.height = renderContext.style.height === "43vh" ? "94vh" : "43vh";
+   codeContext.style.display = codeContext.style.display === "flex" ? "none" : "flex";
+   codingSection.style.height = codingSection.style.height === "56.5vh" ? "5.5vh" : "56.5vh";
+
+
+}
+
+
